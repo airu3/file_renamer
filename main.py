ccadca1136f01f742a959f446a94b6d42def3404
airu3 <sugar.tom5310@gmail.com>
2024-05-02 16:12:01 +0900

    ファイルのリネーム機能の改善と修正

--- main.py
+++ main.py
@@ -1,76 +1,77 @@
 import os
 from datetime import datetime
 
-"""
-前方部分一致でリネームできるようにしたい
-B: typing-2021-0615-1234.png
-A: typing_2021-0615-1234.png
 
-入力してファイルの新しい名前を指定できるようにしたい
-B: typing-2021-0615-1234.png
-A: input()-2021-0615-1234.png
-保存先のフォルダ変更機能を削除したい
-
-"""
+"""文字列を指定した長さに切り詰め、必要に応じて文字を追加する"""
 
 
 def truncate_string(input_string, length, append_string="..."):
-    """文字列を指定した長さに切り詰め、必要に応じて文字を追加する"""
     if len(input_string) > length:
         return input_string[:length] + append_string
     else:
         return input_string
 
 
+"""指定したフォルダから指定した名前を含むファイルのリストを取得する"""
+
+
 def get_files(src_folder, file_name):
-    """指定したフォルダから指定した名前を含むファイルのリストを取得する"""
     files = [file for file in os.listdir(src_folder) if file_name in file]
     src_folder_display = truncate_string(src_folder, 20, "...")
     print(f"Found {len(files)} files containing '{file_name}' in {src_folder_display}.")
     return files
 
 
-def rename_files(src_folder, files, new_file_prefix):
-    """ファイルを新しい名前でリネームする"""
+"""ファイルの作成日時を取得し、指定したフォーマットで返す"""
+
+
+def get_formatted_time(src_path, file, date_format):
+    src_path = os.path.join(src_path, file)
+    creation_time = os.path.getctime(src_path)
+    return datetime.fromtimestamp(creation_time).strftime(date_format)
+
+
+"""ファイルを新しい名前でリネームする"""
+
+
+def rename_files(src_path, files, new_file_prefix):
     for file in files:
-        # ファイルのパスを作成
-        src_path = os.path.join(src_folder, file)
-        # ファイルの作成日時を取得
-        creation_time = os.path.getctime(src_path)
-        # ファイルの作成日時からファイル名を作成
-        formatted_time = datetime.fromtimestamp(creation_time).strftime("%Y-%m%d-%H%M")
+        formatted_time = get_formatted_time(src_path, file, "%Y-%m%d-%H%M%S")
 
         new_file_name = f"{new_file_prefix}{formatted_time}.png"
         # 保存先ファイルのパスを作成
-        dst_path = os.path.join(src_folder, new_file_name)
+        dst_path = os.path.join(src_path, new_file_name)
 
         # ファイルをリネーム
         os.replace(src_path, dst_path)
         # 20文字以上の場合は省略して表示
-        src_folder_display = truncate_string(src_folder, 20, "...")
+        src_path_display = truncate_string(src_path, 20, "...")
         print(
-            f"Renamed file '{file}' to '{new_file_name}'. It is saved in '{src_folder_display}'."
+            f"Renamed file '{file}' to '{new_file_name}'. It is saved in '{src_path_display}'."
         )
+
+
+"""メイン関数"""
 
 
 def main():
     print("Start file renaming process...")
     # ユーザーにフォルダパスを入力させる
-    src_folder = input("Enter the 'path' to search: ")
+    src_path = input("Enter the 'path' to search: ")
     # ユーザーに検索ファイル名を入力させる
     file_name = input("Enter the 'file' to search: ")
 
     # スクリーンショットファイルのリストを取得
-    screenshot_files = get_files(src_folder, file_name)
+    screenshot_files = get_files(src_path, file_name)
 
     # ユーザーに新しいファイル名を入力させる
     new_file_prefix = input("Enter the 'new file name' to rename: ")
 
     # スクリーンショットファイルをリネーム
-    rename_files(src_folder, screenshot_files, new_file_prefix)
+    rename_files(src_path, screenshot_files, new_file_prefix)
     print("File renaming process is complete.")
 
 
-# このスクリプトを直接実行する場合にのみmain関数を呼び出す
+"""このスクリプトを直接実行する場合にのみmain関数を呼び出す"""
 if __name__ == "__main__":
     main()
